00e235134ec21fadba2dbcbfdc747d50bcc0e6ce
DiggerTigger <diggertigger@posteo.de>
2016-08-11 15:52:36 +0200

    Differentiations of a function x with stepsize dx

--- diff.jl
+++ diff.jl
@@ -1,4 +1,16 @@
-#diff.jl
+###diff.jl - Calculate the differentiation of a one-dimensional function###
 
-function diff_1_forward(x,dx)
+# Euler forward algorithm #
+function diff_1_forward(F,x,dx)
+	(F(x+dx)-F(x))/dx
+end
 
+# Euler backwards algorithm #
+function diff_1_backward(F,x,dx)
+	(F(x)-F(x-dx))/dx
+end
+
+# Central differentiation #
+function diff_1_central(F,x,dx)
+	(F(x+dx)-F(x-dx))/2/dx
+end
